850851e3d5875fbdf50a6494b41dff3d0223f676
Rodrigo Gonzalez <r@rodrigo-gonzalez.com>
2023-08-24 16:08:33 -0400

    feat(project): update project completely add poetry, linters, move python files to package, add versioning
    Comprehensive Overhaul of Fraud Detection Package
    Description:
    This commit represents a significant restructuring and enhancement of the fraud-detection package, a project initially developed seven years ago. The changes aim to improve the code quality, maintainability, and alignment with current best practices in Python development. Here's a detailed summary of the changes:
    
    1. Reorganization:
    
    Moved all Python files to a new package structure under src/fraud_detection.
    The restructured directory enables better separation of concerns and aligns with modern packaging practices.
    2. Configuration Files:
    
    Added configuration files for various tools and linters, including .pre-commit-config.yaml, pylama.ini, pytest.ini, setup.cfg, Makefile, LICENSE, and more.
    These files are essential for enforcing coding standards, running tests, and automating common development tasks.
    3. Pyproject.toml Enhancements:
    
    Updated the pyproject.toml file to include essential project metadata, dependencies, and configuration for tools like isort, black, mypy, coverage, and tryceratops.
    Defined the project's name, version, description, authors, and license.
    Specified dependencies for different environments, including development, testing, and documentation.
    Added specific rules for code formatting, type checking, and coverage reporting.
    4. Linting and Version Updates:
    
    Ran all the linters specified in the configuration files to ensure code quality and consistency.
    Updated the versions of dependencies to their latest stable releases.
    5. Packaging Improvements:
    
    Moved all the python files to a new package under src/fraud_detection, aligning with modern packaging practices.
    Ensured that essential files like README.md, LICENSE, and CHANGELOG.md are included in the package.
    6. Overhauled Legacy Code:
    
    The package has been completely overhauled to align with current best practices and technologies.
    Enhancements include code refactoring, dependency updates, and alignment with coding standards.
    Conclusion:
    This overhaul marks a significant milestone in the evolution of the fraud-detection package. By adopting modern development practices and tooling, the project is now better positioned for ongoing maintenance, collaboration, and extension. The changes reflect a commitment to code quality and offer a solid foundation for future development.

--- src/fraud_detection/model.py
+++ src/fraud_detection/model.py
@@ -1,10 +1,10 @@
-import cPickle as pickle
-from build_model import Model
+import pickle
 from os import path
+
 from build_model import build_model
 
 
-def load_model(data_filename='data/train.json', model_filename='model.pkl'):
+def load_model(data_filename="data/train.json", model_filename="model.pkl"):
     if path.isfile(model_filename):
         with open(model_filename) as f:
             model = pickle.load(f)
