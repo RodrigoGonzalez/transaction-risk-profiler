2c3bb090d0cb513cd5a8b56260be2b10464e2947
Rodrigo Gonzalez <r@rodrigo-gonzalez.com>
2023-09-27 18:18:01 -0400

    bump: version 0.3.0 → 0.4.0 (#29)

--- src/transaction_risk_profiler/__init__.py
+++ src/transaction_risk_profiler/__init__.py
@@ -1,6 +1,6 @@
 """ Package initialization. """
 from pathlib import Path
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 APP_DIR = Path(__file__).parent.resolve()
 PROJECT_DIR = APP_DIR.parent.parent.resolve()
